8b60e2ef1220b5a135dbdb03329e8a3bc1c87e20
Matheus Henrique <matheusvita007@gmail.com>
2026-04-11 01:10:42 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/matheushenrique/todosimple/TodosimpleApplication.java
@@ -0,0 +1,12 @@
+package com.matheushenrique.todosimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.matheushenrique.todosimple.TodosimpleApplication.class, args);
+	}
+}

7b9cf9a776bfa429b163d979cbeeaff231860400
Matheus Henrique <matheusvita007@gmail.com>
2026-04-14 19:28:03 -0300

    feat: adicionar o UserService

---
+++ src/main/java/com/matheushenrique/todosimple/services/UserService.java
@@ -0,0 +1,55 @@
+package com.matheushenrique.todosimple.services;
+
+import com.matheushenrique.todosimple.models.User;
+import com.matheushenrique.todosimple.repositories.TaskRepository;
+import com.matheushenrique.todosimple.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public User findById(Long id) {
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(
+                "Usuário não foi encontrado! Id: " + id + ", Tipo: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create(User obj) {
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.taskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj) {
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id) {
+        findById(id);
+        try {
+
+            this.userRepository.deleteById(id);
+
+        } catch (Exception e) {
+
+            throw new RuntimeException("Não é possível excluir devido a dependências existentes!", e);
+
+        }
+    }
+}

cd4b8a42696b37549d110f0fb5d08715c9a73331
Matheus Henrique <matheusvita007@gmail.com>
2026-04-20 15:55:39 -0300

    feat: adiciona os tratamentos de erro

--- src/main/java/com/matheushenrique/todosimple/services/TaskService.java
+++ src/main/java/com/matheushenrique/todosimple/services/TaskService.java
@@ -3,6 +3,8 @@ package com.matheushenrique.todosimple.services;
 import com.matheushenrique.todosimple.models.Task;
 import com.matheushenrique.todosimple.models.User;
 import com.matheushenrique.todosimple.repositories.TaskRepository;
+import com.matheushenrique.todosimple.services.exceptions.DataBindingViolationException;
+import com.matheushenrique.todosimple.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +23,7 @@ public class TaskService {
 
     public Task findById(Long id) {
         Optional<Task> task = this.taskRepository.findById(id);
-        return task.orElseThrow(() -> new RuntimeException(
+        return task.orElseThrow(() -> new ObjectNotFoundException(
                 "Tarefa não encontrada! Id: " + id + ", Tipo: " + Task.class.getName()));
     }
 
@@ -51,7 +53,7 @@ public class TaskService {
         try {
             this.taskRepository.deleteById(id);
         } catch (Exception e) {
-            throw new RuntimeException("Não é possível excluir devido a dependências existentes!", e);
+            throw new DataBindingViolationException("Não é possível excluir devido a dependências existentes!");
         }
     }
 }
--- src/main/java/com/matheushenrique/todosimple/services/UserService.java
+++ src/main/java/com/matheushenrique/todosimple/services/UserService.java
@@ -3,6 +3,8 @@ package com.matheushenrique.todosimple.services;
 import com.matheushenrique.todosimple.models.User;
 import com.matheushenrique.todosimple.repositories.TaskRepository;
 import com.matheushenrique.todosimple.repositories.UserRepository;
+import com.matheushenrique.todosimple.services.exceptions.DataBindingViolationException;
+import com.matheushenrique.todosimple.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = this.userRepository.findById(id);
-        return user.orElseThrow(() -> new RuntimeException(
+        return user.orElseThrow(() -> new ObjectNotFoundException(
                 "Usuário não foi encontrado! Id: " + id + ", Tipo: " + User.class.getName()
         ));
     }
@@ -47,7 +49,7 @@ public class UserService {
 
         } catch (Exception e) {
 
-            throw new RuntimeException("Não é possível excluir devido a dependências existentes!", e);
+            throw new DataBindingViolationException("Não é possível excluir devido a dependências existentes!");
 
         }
     }
